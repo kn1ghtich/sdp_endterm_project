b671c344749919d0251c1502c8091070b0388204
KassiyeTD <154661983+KassiyeTD@users.noreply.github.com>
2024-11-02 21:24:07 +0500

    Kassiyet

---
+++ skaterental/SkateboardFactory.java
@@ -0,0 +1,26 @@
+package skaterental;
+
+import skaterental.boards.Cruiser;
+import skaterental.boards.Longboard;
+import skaterental.boards.Skateboard;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SkateboardFactory {
+    private static Map<String, Skateboard> boards = new HashMap<>();
+
+    public SkateboardFactory(){
+        boards.put("Cruiser", new Cruiser());
+        boards.put("Longboard", new Longboard());
+        boards.put("Classic", new Longboard());
+    }
+
+    public Skateboard createSkateboard(String type) {
+        if (boards.containsKey(type)) {
+            return boards.get(type);
+        } else {
+            throw new IllegalArgumentException("No such board type: " + type);
+        }
+    }
+}
